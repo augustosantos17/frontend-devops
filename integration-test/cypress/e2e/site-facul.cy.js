4ebc37c268cbeba12baa6fec18bb129be815b321
Augusto <augustosantos1709@gmail.com>
2025-04-29 21:04:57 -0300

    update

--- integration-test/cypress/e2e/site-facul.cy.js
+++ integration-test/cypress/e2e/site-facul.cy.js
@@ -1,11 +1,11 @@
 describe('Página Inicial', () => {
   it('Deve carregar corretamente a página', () => {
-    cy.visit('https://domeneghetti-devops-ci.azurewebsites.net/index.html')
+    cy.visit('https://devclass-frontend-devops.azurewebsites.net/cursos.html')
     cy.contains('Faculdade Tech')
   });
 
   it('Deve navegar até a página de Cursos', () => {
-    cy.visit('https://domeneghetti-devops-ci.azurewebsites.net/index.html')
+    cy.visit('https://devclass-frontend-devops.azurewebsites.net/cursos.html')
     cy.get('nav a').contains('Cursos').click();
     cy.url().should('include', 'cursos.html');
   });
